e4a6870e51086b174712dfd880e5f5b4a5069d87
keezeem <azeemolajide7@gmail.com>
2025-02-06 11:39:07 +0100

    Header & Footer components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Header from '@/components/shared/header';
+import Footer from '@/components/footer';
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <div className='flex h-screen flex-col'>
-        <main className='flex-1 wrapper'>{children}</main>
-      </div>
-    );
-  } 
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className='flex h-screen flex-col'>
+      <Header />
+      <main className='flex-1 wrapper'>{children}</main>
+      <Footer />
+    </div>
+  );
+}
