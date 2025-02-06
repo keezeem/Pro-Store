7e32b3012b33c2058cff95d3c667c9ecac2b1b9f
keezeem <azeemolajide7@gmail.com>
2025-02-06 12:30:47 +0100

    Theme mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
-import type { Metadata } from "next";
-import "@/assets/styles/globals.css";
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
-
+import '@/assets/styles/globals.css';
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
+import { ThemeProvider } from 'next-themes';
 
 const inter = Inter({ subsets: ['latin'] });
 
-
-
 export const metadata: Metadata = {
-  
   title: {
     template: `%s | Prostore`,
     default: APP_NAME,
@@ -24,11 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`} 
-      >
-        {children}
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
