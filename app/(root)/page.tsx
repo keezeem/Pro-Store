bcbf27233cfd25332e28895b20b5f604ff4985bd
keezeem <azeemolajide7@gmail.com>
2025-02-06 14:46:43 +0100

    List sample products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
-const Homepage = () => {
-  return (
-    <>
-      Prostore
-    </>
-  )
-}
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
 
-export default Homepage
+const Homepage = () => {
+  if (!sampleData || !sampleData.products) {
+    return <p className="text-red-500">Error: Product data not found.</p>;
+  }
+
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList title="Newest Arrivals" data={sampleData.products} limi={4} />
+    </div>
+  );
+};
+
+export default Homepage;
