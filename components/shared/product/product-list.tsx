867722d6420f734dc157122e179dc4fec0e69f99
keezeem <azeemolajide7@gmail.com>
2025-02-07 00:47:39 +0100

    Product card component

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,23 +1,33 @@
-const ProductList = ({ data, title, limit }: { data: { id: string | number; name: string; limit?:number }[]; title?: string }) => {
-const limitedData = limit ? data.slice(0, limit) : data;
+import ProductCard from './product-card';
+import { Product } from '@/components/shared/product/product-list';
 
-    return (
-      <div className="my-10">
-        {title && <h2 className="h2-bold mb-4">{title}</h2>}
-        {data.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {limitedData.map((product) => (
-              <div>{product.name}</div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            <p>No products found</p>
-          </div>
-        )}
-      </div>
-    );
-  };
-  
-  export default ProductList;
-  
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: Product[];
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className='my-10'>
+      <h2 className='h2-bold mb-4'>{title}</h2>
+      {data.length > 0 ? (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {limitedData.map((product: Product) => (
+            <ProductCard key={product.slug} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
