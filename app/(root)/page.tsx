631c9685f87a1faacc4fce380d492de9b0406c87
keezeem <azeemolajide7@gmail.com>
2025-02-08 13:58:52 +0100

    Products action & latest products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,13 @@
 import ProductList from '@/components/shared/product/product-list';
-import sampleData from '@/db/sample-data';
 
-const Homepage = () => {
-  if (!sampleData || !sampleData.products) {
-    return <p className="text-red-500">Error: Product data not found.</p>;
-  }
+import { getLatestProducts } from '@/lib/actions/product.actions';
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+
 
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title='Newest Arrivals'
-        limit={4}
-      />
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
     </>
   );
 };
